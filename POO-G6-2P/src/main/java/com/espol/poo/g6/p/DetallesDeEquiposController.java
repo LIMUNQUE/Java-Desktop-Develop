98d26e5cf3baec95f108b8a837a2c9fd33d44ace
MielesGerson <gdmieles@espol.edu.ec>
2022-08-22 07:43:09 -0500

    Creación de vista detalle de equipos y elementos

---
+++ POO-G6-2P/src/main/java/com/espol/poo/g6/p/DetallesDeEquiposController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.espol.poo.g6.p;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author Gerson
+ */
+public class DetallesDeEquiposController implements Initializable{
+
+    @FXML
+    private Label lblequipo1;
+    @FXML
+    private Label lblequipo2;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //TODO
+    }
+    
+}
